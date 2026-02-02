255f7543ed0e9ffe900999b84c8cbb2a02887560
lyy323744-cloud <lyy323744@gmail.com>
2026-02-02 19:55:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,37 +19,3 @@ form.addEventListener('submit', e => {
   e.preventDefault();
   alert('تم إرسال طلبك بنجاح!');
 });
-
-// Carousel بالسحب يمين/يسار
-const track = document.querySelector('.carousel-track');
-let isDown = false, startX, scrollLeft;
-
-track.addEventListener('mousedown', (e) => {
-  isDown = true;
-  startX = e.pageX - track.offsetLeft;
-  scrollLeft = track.scrollLeft;
-});
-
-track.addEventListener('mouseleave', () => isDown = false);
-track.addEventListener('mouseup', () => isDown = false);
-track.addEventListener('mousemove', (e) => {
-  if(!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - track.offsetLeft;
-  const walk = (x - startX) * 1;
-  track.scrollLeft = scrollLeft - walk;
-});
-
-// للسحب باللمس (للتابلت والهاتف)
-track.addEventListener('touchstart', e => {
-  isDown = true;
-  startX = e.touches[0].pageX - track.offsetLeft;
-  scrollLeft = track.scrollLeft;
-});
-track.addEventListener('touchend', () => isDown = false);
-track.addEventListener('touchmove', e => {
-  if(!isDown) return;
-  const x = e.touches[0].pageX - track.offsetLeft;
-  const walk = (x - startX) * 1;
-  track.scrollLeft = scrollLeft - walk;
-});
